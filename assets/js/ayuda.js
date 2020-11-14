c8a5dff6ddbc757a29693a3dda1966abf2fcc6e6
Fernanda Mayer <sofi.breitkreitz@gmail.com>
2020-11-14 18:42:50 -0300

    Revert "Update ayuda.js"
    This reverts commit d0aada1619c48aa4f0247614905b8737230539f8.

--- assets/js/ayuda.js
+++ assets/js/ayuda.js
@@ -201,6 +201,11 @@ function armarMuro(pInmueblesAMostrar) {
     for (let i = 0; i < pInmueblesAMostrar.length; i++) {
 
         let inmueble = pInmueblesAMostrar[i];
+
+        
+        if (inmueble.habilitado) {
+
+        let promedio = (inmueble.sumaCalificaciones / inmueble.calificaciones.length).toFixed(1);
         
         muroHtml += `<div>
         <h2>${inmueble.titulo}</h2>
@@ -208,7 +213,7 @@ function armarMuro(pInmueblesAMostrar) {
         <img src="./assets/img/${inmueble.imagenes[0]}" alt="casa de campo">
         <div>
             <label><strong>${inmueble.ciudad}</strong></label><label class="duracion">Promedio:
-                <strong>${inmuebles.promedio}</strong></label>
+                <strong>${promedio}</strong></label>
         </div>
         <p>${inmueble.descripcion}</p>
         <p class="ver-mas" id="verMas${i}">
@@ -216,6 +221,7 @@ function armarMuro(pInmueblesAMostrar) {
         </p>
         <hr>
         </div>`
+        }
     }
 
     document.getElementById('divMuro').innerHTML = muroHtml;
@@ -224,38 +230,12 @@ function armarMuro(pInmueblesAMostrar) {
 
 
 //Funcion validar numero
-function valorNumerico(pNumero) {
+function valorNumerico(_numero){
     let esNumero; 
-    if (!isNaN(pNumero) && pNumero !== ''){
+    if (!isNaN(_numero) && _numero !== ''){
         esNumero = true;
     } else {
         esNumero = false;
     }
     return esNumero;
-}
-
-//Función para sumar elementos de un array de números
-function sumarArray(pArray) {
-    let suma = 0;
-    
-    for (i=0; i < pArray.length; i++) {
-        suma += pArray[i];
-    }
-
-    return suma;
-
-}
-
-//Función para realizar promedios
-function promedio(pSumatoria, pCantidad, pDigitoPosComa) {
-    let promedio = 0;
-
-    //Si no hay valores ingresados devolvemos 0, para no dividir entre 0
-    if (pCantidad === 0) {
-    //Si no hacemos el cálculo correspondiente
-    } else {
-        promedio = (pSumatoria / pCantidad).toFixed(pDigitoPosComa);
-    }
-
-    return promedio;
 }
