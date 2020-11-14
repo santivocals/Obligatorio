284c141a1dff56fd021c2f19fb192b0a7b9e7190
Santiago Migues <santi.vocals@gmail.com>
2020-11-14 19:06:25 -0300

    Update clases.js

--- assets/js/clases.js
+++ assets/js/clases.js
@@ -7,28 +7,35 @@ class Usuario {
 	//Tipo de usuario (Huésped, anfitrion, admin)
 	tipo;
 	activo;
-	//Atributo de huésped
+	//Propiedad de huésped
 	//Se agrega titulo del inmueble al array cuando solicita reserva y se habilita botón de calificar
-	//Una vez califica el inmueble se deshabilita botón de calificar
+	//Una vez califica el inmueble se elimina del array y se deshabilita botón de calificar
 	reservas;
-	//Atributo de anfitrión
+	//Propiedad de anfitrión
 	//Se agregan en array los inmuebles que registre el anfitrión
 	inmuebles;
 
-	constructor(pNombre, pApellido, pCorreo, pCelular, pPassword, pTipo) {
+	constructor(pNombre, pApellido, pCorreo, pCelular, pPassword) {
 		this.nombre = pNombre;
 		this.apellido = pApellido;
 		this.correo = pCorreo;
 		this.celular = pCelular;
 		this.password = pPassword;
-		this.tipo = pTipo;
 		
+<<<<<<< HEAD
 		//Inicializamos atributos sin recibir información por parámetro
+<<<<<<< HEAD
+=======
+		//Ver si corresponde colocarlo como un parámetro, ya que se asigna según la pantalla desde donde se resgistre
+		this.tipo = pTipo;
+
+		//Inicializamos atributos sin recibir información desde fuera
+>>>>>>> parent of f09e5bf... Update clases.js
+=======
+		this.tipo = '';
+>>>>>>> parent of 5ba7bab... Update clases.js
 		this.activo = true;
-		//Array de inmuebles reservados por el Huésped
-		//Lo pusheamos desde la entidad Reserva
 		this.reservas = [];
-		//Array de inmuebles propios del Anfitrión
 		this.inmuebles = [];
 	}
 }
@@ -45,44 +52,25 @@ class Inmueble {
 	promedio;
 	
 
-	constructor(pTitulo, pDescripcion, pCiudad, pPrecio, pImagenes) {
+	constructor(pTitulo, pDescripcion, pCiudad, pPrecio, pImagenes, pAnfitrion, pCalificaciones, pSumaCalificaciones) {
 		this.titulo = pTitulo;
 		this.descripcion = pDescripcion;
 		this.ciudad = pCiudad;
 		this.precio = pPrecio;
 		this.imagenes = pImagenes;
-
-		//Inicializamos atributos sin recibir información por parámetro
 		this.habilitado = true;
+<<<<<<< HEAD
 		//El anfitrión se obtiene desde el usuarioConectado que está creando el inmueble
 		this.anfitrion;
 		//Guardamos en un array las califiaciones que vamos a obtener desde la entidad Reserva
 		this.calificaciones = [];
 		this.promedio = 0;
 		
+=======
+		this.anfitrion = pAnfitrion;
+		//Guardamos en un array todas las calificaciones de usuarios
+		this.calificaciones = pCalificaciones;
+		this.sumaCalificaciones = 0 + pSumaCalificaciones;
+>>>>>>> 6fef864169caad8287db8b1ab9df3c920f78e1cf
 	}
-}
-
-class Reserva {
-	cantNoches;
-	inmueble;
-	costoReserva;
-	calificación;
-
-	constructor(pCantNoches) {
-		this.cantNoches = pCantNoches;
-
-		//Inicializamos atributos sin recibir información por parámetro
-		this.inmueble;
-		this.costoReserva = reserva.cantNoches * inmueble.precio;
-		//ATENCIÓN: ARREGLAR CUANDO HAGAMOS EL MÓDULO DE CALIFICACIÓN, NO SE PUEDE INICIALIZAR EN 0
-		//YA QUE 0 ES UN VALOR VÁLIDO DE CALIFICACIÓN Y NO UNA CALIFICACIÓN NO REALIZADA
-		//QUIZÁ SEA BUENA IDEA INCIALIZAR EN 'FALSE' A UNA VARIABLE CALIFICADO Y MANEJAR LA CALIFICACIÓN COMO
-		//UNA VARIABLE GLOBAL QUE SE GUARDE EN EL ARRAY CALIFICACIONES Y QUE PASE A TRUE EL ATRIBUTO 'CALIFICADO´
-		//EN LA ENTIDAD CUANDO EL USUARIO INGRESE LA CALIFICACIÓN, Y DESHABILIE EL BOTÓN DE CALIFICAR SI EL ATRIBUTO
-		//reserva.calificado es True
-		this.calificación;
-	}
-	
-	
 }
