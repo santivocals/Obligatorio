3fac5c01cecd9f6642e0ce4430ea1d853ef8a289
AgusCola <74433595+AgusCola@users.noreply.github.com>
2020-11-17 23:31:10 -0300

    Revert "Update ayuda.js"
    This reverts commit 20290e5aff8af3b95baffd47dec5dd66bfe909f8.

--- assets/js/ayuda.js
+++ assets/js/ayuda.js
@@ -190,35 +190,65 @@ function armarMuro(pInmueblesAMostrar) {
 
     let muroHtml = "";
 
-    let copiaArray = [...pInmueblesAMostrar];
+    if(criterioOrden === "popularidad"){
 
-    copiaArray.sort(criterioOrdenPopu);
+        pInmueblesAMostrar.sort(criterioOrdenPopu);
 
-    for (let i = 0; i < pInmueblesAMostrar.length; i++) {
-
-        let inmueble = pInmueblesAMostrar[i];
-
-        if (inmueble.habilitado === true){
-        //Asignamos valor a parametro promedio de la entidad Inmueble
-        inmueble.promedio = promedio(sumarArray(inmueble.calificaciones), inmueble.calificaciones.length, 1);
-
-        muroHtml += `<div>
-        <h2>${inmueble.titulo}</h2>
-        <h4><strong>${moneda} ${obtenerPrecio(inmueble.precio)}</strong> por noche</h4>
-        <img src="./assets/img/${inmueble.imagenes[0]}" alt="casa de campo">
-        <div>
-            <label><strong>${inmueble.ciudad}</strong></label><label class="duracion">Promedio:
-                <strong>${inmueble.promedio}</strong></label>
-        </div>
-        <p>${inmueble.descripcion}</p>
-        <p class="ver-mas" id="verMas${i}">
-            Ver más...
-        </p>
-        <hr>
-        </div>`
+        for (let i = 0; i < pInmueblesAMostrar.length; i++) {
+    
+            let inmueble = pInmueblesAMostrar[i];
+    
+            if (inmueble.habilitado === true){
+            //Asignamos valor a parametro promedio de la entidad Inmueble
+            inmueble.promedio = promedio(sumarArray(inmueble.calificaciones), inmueble.calificaciones.length, 1);
+    
+            muroHtml += `<div>
+            <h2>${inmueble.titulo}</h2>
+            <h4><strong>${moneda} ${obtenerPrecio(inmueble.precio)}</strong> por noche</h4>
+            <img src="./assets/img/${inmueble.imagenes[0]}" alt="casa de campo">
+            <div>
+                <label><strong>${inmueble.ciudad}</strong></label><label class="duracion">Promedio:
+                    <strong>${inmueble.promedio}</strong></label>
+            </div>
+            <p>${inmueble.descripcion}</p>
+            <p class="ver-mas" id="verMas${i}">
+                Ver más...
+            </p>
+            <hr>
+            </div>`
+            }
         }
+    } else{
 
+        pInmueblesAMostrar.sort(criterioOrdenPrecio);
+
+        for (let i = 0; i < pInmueblesAMostrar.length; i++) {
+    
+            let inmueble = pInmueblesAMostrar[i];
+    
+            if (inmueble.habilitado === true){
+            //Asignamos valor a parametro promedio de la entidad Inmueble
+            inmueble.promedio = promedio(sumarArray(inmueble.calificaciones), inmueble.calificaciones.length, 1);
+    
+            muroHtml += `<div>
+            <h2>${inmueble.titulo}</h2>
+            <h4><strong>${moneda} ${obtenerPrecio(inmueble.precio)}</strong> por noche</h4>
+            <img src="./assets/img/${inmueble.imagenes[0]}" alt="casa de campo">
+            <div>
+                <label><strong>${inmueble.ciudad}</strong></label><label class="duracion">Promedio:
+                    <strong>${inmueble.promedio}</strong></label>
+            </div>
+            <p>${inmueble.descripcion}</p>
+            <p class="ver-mas" id="verMas${i}">
+                Ver más...
+            </p>
+            <hr>
+            </div>`
+            }
+        }
     }
+
+    
 
     document.getElementById('divMuro').innerHTML = muroHtml;
     document.getElementById('divReporte').innerHTML = muroHtml;
@@ -302,6 +332,10 @@ function btnDesInmuebleHandler(){
     arrayInmuebles[posicion].habilitado = false;
 
 }
+
+
+
+
 
 //Calificación del Inmueble
 
